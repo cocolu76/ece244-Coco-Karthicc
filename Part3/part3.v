e65405ae6cec29e053f05445e855ed3aa41e95bf
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2023-09-24 22:00:59 -0400

    Add files via upload

--- Part3/part3.v
+++ Part3/part3.v
@@ -8,17 +8,23 @@ module hex_decoder(c, display);
 	assign c2 = c[2];
 	assign c3 = c[3];
 
-	assign display[0] = ~c2&~c0 | ~c3&c2&c0 | c2&c1 | c3&~c2&~c1 | c3&~c0;
-	assign display[1] = ~c3&~c1&~c0 | ~c2&~c1 | ~c2&~c0 | ~c3&c1&c0 | c3&~c1&c0;
-	assign display[2] = ~c2&~c1 | ~c2&c0 | ~c1&c0 | ~c3&c2 | c3&~c2;
-	assign display[3] = ~c3&~c2&~c0 | ~c2&c1&c0 | c2&~c1&c0 | c2&c1&~c0 | c3&~c1;
-	assign display[4] = ~c2&~c0 | c1&~c0 | c3&c1 |c3&c2;
-	assign display[5] = ~c1&~c0 | ~c3&c2&~c1 | c2&~c0 | c3&~c2 | c3&c1;
-	assign display[6] = ~c2&c1 | c1&~c0 | ~c3&c2&~c1 | c3&~c2 | c3&c0;
+	wire [6:0] inverted; //I messed up, everything is inverted
+
+	assign inverted[0] = ~c2&~c0 | ~c3&c2&c0 | c2&c1 | c3&~c2&~c1 | c3&~c0 | ~c3&c1;
+	assign inverted[1] = ~c3&~c1&~c0 | ~c2&~c1 | ~c2&~c0 | ~c3&c1&c0 | c3&~c1&c0;
+	assign inverted[2] = ~c2&~c1 | ~c2&c0 | ~c1&c0 | ~c3&c2 | c3&~c2;
+	assign inverted[3] = ~c3&~c2&~c0 | ~c2&c1&c0 | c2&~c1&c0 | c2&c1&~c0 | c3&~c1;
+	assign inverted[4] = ~c2&~c0 | c1&~c0 | c3&c1 |c3&c2;
+	assign inverted[5] = ~c1&~c0 | ~c3&c2&~c1 | c2&~c0 | c3&~c2 | c3&c1;
+	assign inverted[6] = ~c2&c1 | c1&~c0 | ~c3&c2&~c1 | c3&~c2 | c3&c0;
+	
+	assign display = ~inverted;
 
 endmodule
 
 //*********for testing*************************************
-module hex(input SW[3:0], output HEX0[6:0])
-	hex_decoder H1(SW, HEX0);
+module hex(SW, HEX0);
+	input [3:0] SW;
+	output [6:0] HEX0;
+	hex_decoder H1(SW, HEX0);	
 endmodule
